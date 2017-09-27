dd6ad113f793a98e827902c30fc01cc127ebf3cd
dennyzhang <denny@dennyzhang.com>
2017-09-27 15:02:18 -0500

    add precheck

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,21 @@
 #!/bin/bash -e
 cd /code
 
+# Precheck
+if [ ! -f config.py ]; then
+    echo "ERROR: config.py is not found. See sample in config.py.template"
+    exit 1
+fi
+
+if [ ! -f ssh_id_rsa ]; then
+    echo "Warning: ssh_id_rsa is not found. Some slack commands may not work."
+fi
+
+################################################################################
 echo "remove pyc files"
 find . -type f -name "*.pyc" -print0 | xargs rm -rf
 
-# TODO: verify config.py is configured
-if [ ! -f config.py ]; then
-    echo "ERROR: config.py"
-fi
-
-# TODO: show warning, if ssh key file is not there
-
+# Start the service
 uwsgi -w wsgi --http-socket :8081 --enable-threads --vacuum --logto /tmp/uwsgi-wsgi.log
 # tail -f /tmp/uwsgi-wsgi.log
-
 ## File : docker-entrypoint.sh ends
